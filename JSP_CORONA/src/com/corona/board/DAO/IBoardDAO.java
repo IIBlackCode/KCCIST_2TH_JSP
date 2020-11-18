adcd2f936e68db3ca84e584e10b72f6c8bfc947d
MASTER <MASTER@DESKTOP-2OVON5I>
2020-11-19 02:41:44 +0900

    DataBaseCOnnection 추상화 실패, 추후 다시 시도 예정	By MinSeo

--- JSP_CORONA/src/com/corona/board/DAO/IBoardDAO.java
+++ JSP_CORONA/src/com/corona/board/DAO/IBoardDAO.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.List;
 
 import com.corona.DataBaseConnection;
 import com.corona.board.DTO.Board;
