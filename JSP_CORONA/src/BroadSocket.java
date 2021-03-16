d3867dc0663ff8d56d2e3cd636dedb31f1ca753f
MASTER <MASTER@DESKTOP-2A3NTFS>
2021-03-16 16:22:15 +0900

    웹소켓 수정 내역
    실시간 날짜 콘솔 추가
    message > text로 변수명 변경
    text로부터 id와 message 분리
    콘솔에 출력되는 message 내용 수정

--- JSP_CORONA/src/BroadSocket.java
+++ JSP_CORONA/src/BroadSocket.java
@@ -1,6 +1,8 @@
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
  
@@ -15,12 +17,20 @@ public class BroadSocket {
     private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
     
     @OnMessage
-    public void onMessage(String message, Session session) throws IOException {
-        System.out.println(message);
+    public void onMessage(String text, Session session) throws IOException {
+    	
+    	int index = text.indexOf("|");
+    	String id = text.substring(0,index);
+    	String message = text.substring(index+1);
+    	
+    	Date date = new Date(System.currentTimeMillis());
+    	SimpleDateFormat inputDateFormat = new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss] ");
+    	
+        System.out.println(inputDateFormat.format(date)+" <"+id+"> "+message);
         synchronized(clients) {
             for(Session client : clients) {
                 if(!client.equals(session)) {
-                    client.getBasicRemote().sendText(message);
+                    client.getBasicRemote().sendText(text);
                 }
             }
         }
